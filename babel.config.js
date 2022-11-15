80bebe15a72d927e9d358fb3e635d993cec82f0a
Govind Maheshwari <mm2406402@gmail.com>
2022-11-15 11:40:31 +0530

    Add files via upload

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,32 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        cwd: 'babelrc',
+        extensions: [
+          '.ts',
+          '.tsx',
+          '.js',
+          'jsx',
+          '.android.js',
+          '.ios.js',
+          '.json',
+          '.native',
+        ],
+        alias: {
+          app: './src/App.tsx',
+          assets: './src/assets',
+          components: './src/components',
+          constants: './src/constants',
+          controllers: './src/controllers',
+          navigation: './src/navigations',
+          screens: './src/screens/index.ts',
+          utils: './src/utils',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
